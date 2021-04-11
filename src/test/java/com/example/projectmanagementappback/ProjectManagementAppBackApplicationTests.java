86cb808dfc6fa71e06330a83879b5cb9553ba648
Barbu Ionut Alexandru <barbualexandru1997@gmail.com>
2021-04-11 13:14:35 +0300

    Initial commit

---
+++ src/test/java/com/example/projectmanagementappback/ProjectManagementAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectmanagementappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementAppBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
